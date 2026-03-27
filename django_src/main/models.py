809687d86890ef25f7ae729374f4b9e4ebd126a1
Ajvillan <ajvillan6@gmail.com>
2026-03-26 23:59:07 -0400

    Initializing models

--- django_src/main/models.py
+++ django_src/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
 # Create your models here.
+class Alarm(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    time = models.TimeField()
+    label = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.label} at {self.time}"
+    
+    def get_next_due(alarm):
+        now = datetime.now()
+
+        due_today = now.replace(
+            hour=alarm.time.hour,
+            minute=alarm.time.minute,
+            second=0,
+            microsecond=0
+        )
+
+        if due_today > now:
+            return due_today
+        return due_today + timedelta(days=1)
+    
+    def is_due(self):
+        next_due = self.get_next_due()
+        return datetime.now() >= next_due
